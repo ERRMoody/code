b8c53556639b8adad20848f1f78d8704d0a0e98a
Edmund Moody <em16077@it038094.users.bris.ac.uk>
2019-06-24 14:37:15 +0100

    update

--- pgls/pgls.R
+++ pgls/pgls.R
@@ -1,9 +1,8 @@
 library(caper)
 
 arcData <- read.csv("arcfeatures2.csv")
-arcTree2 <- read.tree("rootedlggf.tre")
 arcTree <- read.tree("arcorthocleaned.tre")
-arc <- comparative.data(phy = arcTree2, data = arcData, names.col = 'Species',vcv = TRUE, na.omit = FALSE, warn.dropped = TRUE)
+arc <- comparative.data(phy = arcTree, data = arcData, names.col = 'Species',vcv = TRUE, na.omit = FALSE, warn.dropped = TRUE)
 
 model_pgls<-pgls(OGT ~ prot_D + prot_G + prot_M + prot_P + prot_R + prot_S + prot_V + prot_Y + Genome_Dinuc_AG + Genome_Dinuc_GT + AGA + ATC + CCC + CCT + CGG + prot_Thermolabile + S_GC, data = arc, lambda='ML')
 
@@ -27,8 +26,10 @@ res_squared <- residual ** 2
 
 sum_res <- sum(res_squared)
 
-MSE <- sum_res/length(res_squared)
+MSE <- as.numeric(sum_res/nrow(arcData))
 
 RMSE <- MSE ** 0.5
+
+RMSE
 
 
